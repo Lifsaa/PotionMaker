ff266c20ef91f977fead0d1fb4db7f9c6eadf7e4
Lifsaa <sanogosalif2003@gmail.com>
2024-09-29 22:52:38 -0700

    Fix

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -24,24 +24,25 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
 
 @router.post("/plan")
 def get_bottle_plan():
-    """
-    Go from barrel to bottle.
-    """
-
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory WHERE id = 1")).fetchone()
-    if result is not None:
-        num_green_ml = result.num_green_ml
-    else:
-        num_green_ml  = 0 
-    if num_green_ml > 0:
-        potions_created = num_green_ml // 100
-        remaining_ml = num_green_ml % 100
-    with db.engine.begin() as connection:
+        num_green_ml = result.num_green_ml if result is not None else 0    
+        potions_created = 0
+        remaining_ml = 0      
+        if num_green_ml > 0:
+            potions_created = num_green_ml // 100
+            remaining_ml = num_green_ml % 100
+            
             connection.execute(sqlalchemy.text(
                 "UPDATE global_inventory SET num_green_ml = :remaining_ml, num_green_potions = num_green_potions + :potions_created WHERE id = 1"
             ), {"remaining_ml": remaining_ml, "potions_created": potions_created})
-        
+    return [
+        {
+            "potion_type": [0, 0, 100, 0],
+            "quantity": potions_created,
+        }
+    ]
+
     # Each bottle has a quantity of what proportion of red, blue, and
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
