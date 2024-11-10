72fc3cfb652adf76dc3d1d439e505f7bb5bec656
Lifsaa <sanogosalif2003@gmail.com>
2024-11-09 16:01:05 -0800

    added reflection & query builder

--- src/database.py
+++ src/database.py
@@ -1,8 +1,14 @@
 import os
 import dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,MetaData,Table,Column,Integer,String,Text,ForeignKey,DateTime,func,Index
 
 def database_connection_url():
     dotenv.load_dotenv()
     return os.environ.get("POSTGRES_URI")
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
+metadata = MetaData()
+
+customer_info = Table('customer_info', metadata, autoload_with=engine)
+potion_catalog = Table('potion_catalog', metadata, autoload_with=engine)
+carts = Table('carts', metadata, autoload_with=engine)
+carts_items = Table('carts_items', metadata, autoload_with=engine)
